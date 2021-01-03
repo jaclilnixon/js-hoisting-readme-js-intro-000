210b3386fed041c50ea504e895a67652e6ed0d51
Jacqueline Nixon <jacquelinenixon@me.com>
2021-01-03 06:32:37 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -10,8 +10,7 @@ function callMe() {
 
 function crazy() {
   // fix the code in here:
-  let callMe = () => 'maybe'
-  let crazy = () => console.log("hey!!!")
+  
   thisIsCrazy();
 
   var thisIsCrazy = function (){
